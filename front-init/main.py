614ab30ed9fdd5d691acc7db1cf2e006771927a1
DajanaD <ccv080480@gmail.com>
2024-02-14 13:12:40 +0200

    add save_data

--- front-init/main.py
+++ front-init/main.py
@@ -1,54 +1,54 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
-from pathlib import pathlib, Path
+from pathlib import Path
 import urllib.parse
 import mimetypes
 import socket
-from time import sleep
-import threading
-import datetime
+from datetime import datetime
+from threading import Thread
+import logging
+import json
+
+BASE_DIR = Path()  
+PORT_HTTP = 3000
+HOST_HTTP = "0.0.0.0"
+SOCKET_HOST = "127.0.0.1"
+SOCKET_PORT = 5000
+BUFFER_SIZE = 1024
 
 class HttpHandler(BaseHTTPRequestHandler):
-
-    BASE_DIR = Path()  
-    PORT_HTTP = 3000
-    HOST_HTTP = "0.0.0.0"
-    SOCKET_HOST = "127.0.0.1"
-    SOCKET_PORT = 5000
-    BUFFER_SIZE = 1024
-
-
     def do_POST(self):
-        data = self.rfile.read(int(self.headers['Content-Length']))
-        print(data)
-        data_parse = urllib.parse.unquote_plus(data.decode())
-        print(data_parse)
-        data_dict = {key: datetime.now() for key, value in {key: value for key, value in [el.split('=') for el in data_parse.split('&')]}}
-        print(data_dict)
+        size = self.headers.get("Content-Length")
+        data = self.rfile.read(int(size))
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        client_socket.sendto(data, (SOCKET_HOST, SOCKET_PORT))
+        client_socket.close()
         self.send_response(302)
         self.send_header('Location', '/')
         self.end_headers()
 
     
     def do_GET(self):
-        pr_url = urllib.parse.urlparse(self.path)
-        if pr_url.path == '/':
-            self.send_html_file('index.html')
-        elif pr_url.path == '/contact':
-            self.send_html_file('contact.html')
-        else:
-            if pathlib.Path().joinpath(pr_url.path[1:]).exists():
-                self.send_static()
-            else:
-                self.send_html_file('error.html', 404)
+        route = urllib.parse.urlparse(self.path)
+        match route.path:
+            case "/":
+                self.send_html("index.html")
+            case "/message":
+                self.send_html("message.html")
+            case _:
+                file = BASE_DIR.joinpath(route.path[1:])
+                if file.exists():
+                    self.send_static(file)
+                else:
+                    self.send_html("error.html", 404)
 
-    def send_html_file(self, filename, status=200):
+    def send_html(self, filename, status=200):
         self.send_response(status)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
         with open(filename, 'rb') as fd:
             self.wfile.write(fd.read())
     
-    def send_static(self):
+    def send_static(self, file):
         self.send_response(200)
         mt = mimetypes.guess_type(self.path)
         if mt:
@@ -59,27 +59,43 @@ class HttpHandler(BaseHTTPRequestHandler):
         with open(f'.{self.path}', 'rb') as file:
             self.wfile.write(file.read())
 
+def save_data(data):
+    data_parse = urllib.parse.unquote_plus(data.decode())
+    data_dict = {key: value for key, value in [el.split('=') for el in data_parse.split('&')]}
+    new_dict = {datetime.now(): data_dict,}
+    with open('storage\data.json', "w") as fh:
+        json.dump(new_dict, fh)
+                       
 
-def run(server_class=HTTPServer, handler_class=HttpHandler):
-    server_address = ('', 3000)
-    http = server_class(server_address, handler_class)
+def run_socket_server(host, port):
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server_socket.bind((host, port))
+    logging.info("Starting socket")
     try:
-        http.serve_forever()
+        while True:
+            msg, address = server_socket.recvfrom(BUFFER_SIZE)
+            logging.info("++")
+            save_data(msg)
     except KeyboardInterrupt:
-        http.server_close()
+        server_socket.close()
 
-    HOST = '127.0.0.1'
-    PORT = 5000
+def run_http_server(host, port):
+    address = (host, port)
+    http_server = HTTPServer(address, HttpHandler)
+    logging.info("Starting http")
+    try:
+        http_server.serve_forever()
+    except KeyboardInterrupt:
+        http_server.server_close()
 
-    server = threading.Thread(target=echo_server, args=(HOST, PORT))
-    client = threading.Thread(target=simple_client, args=(HOST, PORT))
 
-    server.start()
-    client.start()
-    server.join()
-    client.join()
-    print('Done!')
 
 if __name__ == '__main__':
-    run()
+    logging.basicConfig(level=logging.DEBUG, format="%(threadName)s%(message)s")
+
+    ser_http = Thread(target=run_http_server, args=(HOST_HTTP, PORT_HTTP))
+    ser_http.start()
+
+    ser_socker = Thread(target=run_socket_server, args=(SOCKET_HOST, SOCKET_PORT))
+    ser_socker.start()
 
